b61f47b45ed73de31aaa6bda86cc38d041116c7e
Andrew-Veteto <vetetoandrew@gmail>
2023-03-02 21:23:39 -0500

    change how div appears

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 function showP1() {
-  document.getElementById("pp2").style.visibility = "hidden";
-  document.getElementById("pp3").style.visibility = "hidden";
-  document.getElementById("pp1").style.visibility = "visible";
+  document.getElementById("pp2").style.display = "none";
+  document.getElementById("pp3").style.display = "none";
+  document.getElementById("pp1").style.display = "block";
 }
 
 function showP2() {
-  document.getElementById("pp1").style.visibility = "hidden";
-  document.getElementById("pp3").style.visibility = "hidden";
-  document.getElementById("pp2").style.visibility = "visible";
+  document.getElementById("pp1").style.display = "none";
+  document.getElementById("pp3").style.display = "none";
+  document.getElementById("pp2").style.display = "block";
 }
 
 function showP3() {
-  document.getElementById("pp1").style.visibility = "hidden";
-  document.getElementById("pp2").style.visibility = "hidden";
-  document.getElementById("pp3").style.visibility = "visible";
+  document.getElementById("pp1").style.display = "none";
+  document.getElementById("pp2").style.display = "none";
+  document.getElementById("pp3").style.display = "block";
 }
 
 function hideVideo2() {
-  document.getElementById("videoBox2").style.visibility = "hidden";
-  document.getElementById("videoBox1").style.visibility = "visible";
+  document.getElementById("videoBox2").style.display = "none";
+  document.getElementById("videoBox1").style.display = "block";
 }
 
 function hideVideo1() {
-    document.getElementById("videoBox1").style.visibility = "hidden";
-    document.getElementById("videoBox2").style.visibility = "visible";
+    document.getElementById("videoBox1").style.display = "none";
+    document.getElementById("videoBox2").style.display = "block";
   }
